76d4edf1735ae56f4a5ecb54426853e93e8de29d
arsh9295 <utkarsh.srivastava95@gmail.com>
2021-01-26 23:19:49 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# awsregion accesskey secretkey awsbucketname
+variable "awsregion" {
+  type = string
+}
+variable "accesskey" {
+  type = string
+}
+variable "secretkey" {
+  type = string
+}
+variable "awsbucketname" {
+  type = string
+}
